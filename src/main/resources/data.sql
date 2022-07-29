3e180de6c41d445759bda8661b24f3c0fa57ca07
Sumit_Kumar <sumit15392@gmail.com>
2022-07-29 23:43:38 +0530

    Submitting Assignment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into users values(1, '$2a$12$dAfBb1v/oQfO4RQp20M7SugBqfFS6Or9CMZcSheBANbSszhOh8kpS','temp');
+insert into roles values (1, 'ADMIN');
+insert into users_roles values (1,1);
+insert into employee values(1, 'ram','parwesh','ram@gl.com');
